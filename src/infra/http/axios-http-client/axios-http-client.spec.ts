68a3ec7a6be8485fbe332283f1cda1f92159c12f
Vinicius Alvim <vini0191@gmail.com>
2021-02-13 20:38:25 -0300

    test: ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -8,10 +8,10 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 const makeSut = (): AxiosHttpClient => new AxiosHttpClient();
 
 describe('AxiosHttpClient', () => {
-  test('should call Axios with correct URL', async () => {
+  test('should call Axios with correct URL and verb', async () => {
     const url = faker.internet.url();
     const sut = makeSut();
     await sut.post({ url });
-    expect(mockedAxios).toHaveBeenCalledWith(url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(url);
   });
 });
